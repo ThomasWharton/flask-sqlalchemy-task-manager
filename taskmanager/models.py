e53ba40c9f473b2a623a39473a99d32363e41f31
ThomasWharton <thomaswharton7@hotmail.com>
2023-07-27 20:49:49 +0000

    Feature: Form for adding new category

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,7 +5,7 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    task = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)  # noqa
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -14,12 +14,12 @@ class Category(db.Model):
 
 class Task(db.Model):
     # schema for the Task model
-    d = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)  # noqa
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
